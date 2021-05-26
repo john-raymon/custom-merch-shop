00b74bf497e498f71cd7d5ee9bdc0549ae327952
John Ray <john@trychec.com>
2021-05-25 23:38:25 -0400

    fix broken lite-react-ui import

---
+++ components/OrderStepsForm.js
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+import { TextField } from 'lite-react-ui';
+
+export default function OrderStepForm(props) { 
+
+  return (
+    <form>
+      {/* // all steps go in here */}
+      <TextField />
+    </form>
+  )
+} 
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import 'lite-react-ui/css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
