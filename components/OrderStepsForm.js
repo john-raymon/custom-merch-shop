04ffc4f5b327d90071f215b3f1472f7328a9d5ad
John Ray <john@trychec.com>
2021-05-26 02:54:04 -0400

    ..

--- components/OrderStepsForm.js
+++ components/OrderStepsForm.js
@@ -1,12 +1,37 @@
 import { useEffect } from 'react';
-import { TextField } from 'lite-react-ui';
 
 export default function OrderStepForm(props) { 
 
   return (
-    <form>
+    <form className="w-full h-full">
       {/* // all steps go in here */}
-      <TextField />
+      <div className="flex w-full">
+        <div className="flex h-full w-1/2">
+          <p className="w-3/4 font-quest text-6xl text-left">
+            Choose a product to design
+          </p>
+        </div>
+        <div className="flex w-1/2">
+          <ul>
+            <li>
+              <div>
+                <input name="product-type" type="radio" />
+                {/* <label for="huey">s</label> */}
+              </div>
+            </li>
+            <li>
+              <div>
+                <input name="product-type" type="radio" />
+              </div>
+            </li>
+            <li>
+              <div>
+                <input name="product-type" type="radio" />
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </form>
   )
-} 
+}
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,22 @@ import OrderStepForm from './../components/OrderStepsForm';
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full p-12 md:p-36 flex h-screen items-center justify-center">
       <OrderStepForm />
     </div>
   )
 }
+// TODO : move serverSide data fetching logic to respective step
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch('https://api.printful.com/products', {
+    headers: new Headers({
+      "Authorization": `Basic ${Buffer.from(`${process.env.PRINTFUL_API_KEY}:`).toString('base64')}`
+    }),
+  })
+  const data = await res.json()
+
+  // Pass data to the page via props
+  return { props: { data } }
+}
+
